0bfb17db1a170f0b4dbfa371044e15e3612d2182
Marathon <3045130265@qq.com>
2026-01-22 22:16:06 +0800

    std::string 待更

---
+++ 26_1_21/test.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//int main()
+//{
+//	try
+//	{
+//
+//	}
+//	catch (const exception& e)
+//	{
+//		cout << e.what() << endl;
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	string s1;
+//	string s2("marathon 666");
+//	string s3(s2);
+//
+//	cout << s1 << endl;
+//	cout << s2 << endl;
+//	cout << s3 << endl;
+//
+//	cin >> s1;
+//	cout << s1;
+//
+//	return 0;
+//}
