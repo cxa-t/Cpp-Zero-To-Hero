af7045d82f8c4f6cd6209c81cba49e2369ab085a
Marathon <3045130265@qq.com>
2026-02-07 18:26:21 +0800

    string收尾

---
+++ 26_2_6/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	char str[] = "牛马";
+//	char16_t str16[] = u"牛马";
+//	char32_t str32[] = U"牛马";
+//	wchar_t wstr[] = L"牛马";
+//	
+//	cout << sizeof(str) << endl;
+//	cout << sizeof(str16) << endl;
+//	cout << sizeof(str32) << endl;
+//	cout << sizeof(wstr) << endl;
+//	return 0;
+//} 
